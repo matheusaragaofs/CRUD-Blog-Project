2c44827fb705f8340d7c00868ebec70cf156664a
matheusaragaofs@gmail.com <matheusaragaofs@gmail.com>
2020-12-01 11:16:51 -0300

    adicionei o tinymce, e começando o andamento com a tabela articles

--- articles/ArticleController.js
+++ articles/ArticleController.js
@@ -1,14 +1,34 @@
 const express = require('express')
 const Router = express.Router()
+const Category = require('../categories/Category')
+const Article =  require('./Article')
+const slugify = require('slugify')
 
-Router.get("/articles",(req,res)=>{
+Router.get("/admin/articles",(req,res)=>{
     res.send('Rota de artigos')
+
 })
 
 Router.get('/admin/articles/new',(req,res)=>{
-    res.send('Rota para criar uam nova artigo')
+    Category.findAll().then(categories=>{
+        res.render('admin/articles/new',{
+            categories:categories
+        })
+        
+    })
 })
 
+Router.post('/articles/save',(req,res)=>{
+    const { title, body, category} = req.body
+    Article.create({
+        title:title,
+        slug: slugify(title),
+        body:body,
+        categoryId: category
+    }).then(()=>{
+        res.redirect('/admin/articles')
+    })
 
+})
 
 module.exports = Router
--- database/database.js
+++ database/database.js
@@ -1,7 +1,8 @@
 const Sequelize =  require('sequelize')
 const connection = new Sequelize('blogcrud','root','matheus@951478',{
     host: 'localhost',
-    dialect: 'mysql'
-})
+    dialect: 'mysql',
+    timezone:"-3:00"  //Brazil, é só procurar a timezone no wikipedia
+ })
 
 module.exports = connection
